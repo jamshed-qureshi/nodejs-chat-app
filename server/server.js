c042367e59f83b986b1890fce75de6fa98674e96
jamshed-qureshi <jamshed.alam_qureshi@yahoo.com>
2018-05-28 11:43:26 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(port, function() => {
-	console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
